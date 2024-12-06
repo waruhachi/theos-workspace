6a377e483aa6baceadf94d65e56e14ebd6000b97
waru <156133757+waruhachi@users.noreply.github.com>
2024-12-06 09:48:42 +0000

    [feat] add Swift install

--- .devcontainer/postInstall.sh
+++ .devcontainer/postInstall.sh
@@ -2,6 +2,11 @@
 
 set -e
 
+SWIFT_VERSION="6.0.2"
+SWIFT_PLATFORM="ubuntu2204"
+SWIFT_PLATFORM_VERSION="ubuntu22.04"
+SWIFT_INSTALL_DIR="$HOME/swift"
+
 SDK_REPO_URL="https://github.com/theos/sdks/archive/master.zip"
 SDK_DEST_DIR="$THEOS/sdks"
 SDK_TMP_DIR=$(mktemp -d)
@@ -13,6 +18,40 @@ cleanup() {
     echo "Cleaning up temporary files..."
     rm -rf "$SDK_TMP_DIR"
     rm -rf "$LIBGC_TMP_DIR"
+}
+
+installSwift() {
+    echo "Installing Swift dependencies..."
+
+    sudo apt-get update
+    sudo apt-get install -y \
+        binutils \
+        git \
+        gnupg2 \
+        libc6-dev \
+        libcurl4-openssl-dev \
+        libedit2 \
+        libgcc-11-dev \
+        libpython3-dev \
+        libsqlite3-0 \
+        libstdc++-11-dev \
+        libxml2-dev \
+        libz3-dev \
+        pkg-config \
+        python3-lldb-13 \
+        tzdata \
+        unzip \
+        zlib1g-dev
+
+    echo "Downloading Swift $SWIFT_VERSION for $SWIFT_PLATFORM..."
+    curl -L "https://download.swift.org/swift-$SWIFT_VERSION-release/$SWIFT_PLATFORM/swift-$SWIFT_VERSION-RELEASE/swift-$SWIFT_VERSION-RELEASE-$SWIFT_PLATFORM_VERSION.tar.gz" -o "/tmp/swift-$SWIFT_VERSION-$SWIFT_PLATFORM_VERSION.tar.gz"
+
+    echo "Extracting Swift archive..."
+    tar xzf "/tmp/swift-$SWIFT_VERSION-$SWIFT_PLATFORM_VERSION.tar.gz" -C "$SWIFT_INSTALL_DIR"
+
+    echo "Adding Swift to PATH..."
+    export PATH="$SWIFT_INSTALL_DIR/usr/bin:$PATH"
+    echo "Swift $SWIFT_VERSION has been installed successfully."
 }
 
 installSDK() {
@@ -59,5 +98,6 @@ installLibGC() {
 
 trap cleanup EXIT
 
+installSwift
 installSDK
 installLibGC
